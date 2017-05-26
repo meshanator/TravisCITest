ec8045c5dddbb97adc7a7bd77f662738f95ce3d2
Meshan Naidoo <meshann@gmail.com>
2017-05-26 10:11:17 +0200

    Add general math test

---
+++ TravisCITest.Web.Tests/General/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TravisCITest.Web.Tests.General
+{
+	[TestClass]
+	public class Math
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var first = 1;
+			var second = 2;
+			Assert.AreEqual(first + second, 3);
+		}
+	}
+}
